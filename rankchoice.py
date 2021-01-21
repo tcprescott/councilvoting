fd7df33a80c3d730747faf69a55581a087bca656
Thomas Prescott <tcprescott@gmail.com>
2021-01-20 21:23:37 -0600

    updates

--- rankchoice.py
+++ rankchoice.py
@@ -11,6 +11,8 @@ import pyrankvote
 load_dotenv()
 
 CANDIDATE_REGEX = re.compile('Ranked Choice \[(.*)\]')
+SEATS = 6
+
 
 def main():
     gc = gspread.authorize(get_creds())
@@ -25,25 +27,26 @@ def main():
 
     ballots = []
     for raw_ballot in raw_ballots:
-        ballot = sorted({candidates[CANDIDATE_REGEX.search(c).group(1)]:r for (c, r) in raw_ballot.items() if CANDIDATE_REGEX.search(c)}.items(), key=lambda x: x[1])
+        ballot = sorted({candidates[CANDIDATE_REGEX.search(c).group(1)]: r for (c, r) in raw_ballot.items() if CANDIDATE_REGEX.search(c)}.items(), key=lambda x: x[1])
         ballots.append(pyrankvote.Ballot(ranked_candidates=[c for c, r in ballot]))
         # print(ballot)
 
-    
-    election_result = pyrankvote.single_transferable_vote(candidates=candidates.values(), ballots=ballots, number_of_seats=7)
+    election_result = pyrankvote.single_transferable_vote(candidates=candidates.values(), ballots=ballots, number_of_seats=SEATS)
 
     winners = election_result.get_winners()
     with open('output/results.txt', 'w+') as f:
         f.write(str(election_result))
-    
+
     print(election_result)
+
 
 def get_creds():
     return Credentials.from_service_account_info(
-            json.loads(os.environ['gsheet_api_oauth'], strict=False),
-            scopes=['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive',
+        json.loads(os.environ['gsheet_api_oauth'], strict=False),
+        scopes=['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive',
                 'https://www.googleapis.com/auth/spreadsheets']
-        )
+    )
+
 
 if __name__ == "__main__":
     main()
